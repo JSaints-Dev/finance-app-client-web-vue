9a940b7fe20da74ace4ec205c81a2cee0815e28f
Jean Saints <57118447+JSaints-Dev@users.noreply.github.com>
2024-10-21 18:05:08 -0300

    chore: update style configs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Theme from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,17 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Theme,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+})
 app.use(router)
 
 app.mount('#app')
