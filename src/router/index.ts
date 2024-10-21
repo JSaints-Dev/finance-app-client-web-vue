1e6506a2aa896082d612934078fc2dc7f327b6d4
Jean Saints <57118447+JSaints-Dev@users.noreply.github.com>
2024-10-21 15:39:52 -0300

    feat: add auth store and router to login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,38 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      redirect: () => {
+        const authStore = useAuthStore()
+        if (authStore.isAuthenticated) {
+          return {
+            name: 'dashboard',
+          }
+        } else {
+          return {
+            name: 'login',
+          }
+        }
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      beforeEnter: (to, from, next) => {
+        const authStore = useAuthStore()
+        if (authStore.isAuthenticated) {
+          next({ name: 'dashboard' })
+        } else {
+          next()
+        }
+      },
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ src/stores/auth.ts
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    accessToken: null as string | null,
+  }),
+  actions: {
+    setAccessToken(token: string) {
+      this.accessToken = token
+    },
+    clearAccessToken() {
+      this.accessToken = null
+    },
+  },
+  getters: {
+    isAuthenticated: state => !!state.accessToken,
+  },
+})
